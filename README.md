db07413c66f2f0ccb1ab365c762a97eb4debb925
jaderaposa <jaduya678@gmail.com>
2023-12-26 14:19:02 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Raposa-Relationships
+ 
---
+++ app/Models/Comment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'student_id',
+        'comment',
+    ];
+
+    public function student()
+    {
+        // return $this->belongsTo('Model', 'foreign_key', 'owner_key');
+        return $this->belongsTo('App\Models\Student', 'student_id', 'id');
+    }
+}
